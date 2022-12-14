bdd7ba63490c8deefda7ab5746624594de0f9ae2
Mohammad Fatemi <mr.smf8@gmail.com>
2022-12-14 15:09:04 +0330

    feat: Add v2ray installation config

--- app/v2ray.sh
+++ app/v2ray.sh
@@ -15,6 +15,17 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
         echo "done installing v2ray. now modify config file in /usr/local/etc/v2ray/config.json"
 
+        sudo cp config-foreign.yaml /usr/local/etc/v2ray/config.json
+
+        sed "s/SS_PASSWORD/${SS_PASSWORD}/g" /usr/local/etc/v2ray/config.json
+        sed "s/VMESS_UUID_NGINX/${VMESS_UUID_NGINX}/g" /usr/local/etc/v2ray/config.json
+        sed "s/VMESS_UUID_WS_1/${VMESS_UUID_WS_1}/g" /usr/local/etc/v2ray/config.json
+        sed "s/VMESS_UUID_WS_2/${VMESS_UUID_WS_2}/g" /usr/local/etc/v2ray/config.json
+        sed "s/VMESS_UUID_TCP_1/${VMESS_UUID_TCP_1}/g" /usr/local/etc/v2ray/config.json
+        sed "s/VMESS_UUID_TCP_2/${VMESS_UUID_TCP_2}/g" /usr/local/etc/v2ray/config.json
+
+        sudo systemctl restart v2ray
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "installing v2ray macOS"
 
