206f9997535bf6e29ab9c8b6d4c0697e12cd731a
Mohammad Fatemi <mr.smf8@gmail.com>
2022-12-23 00:40:38 +0330

    fix: Fix hiddify installation script

--- app/hiddify.sh
+++ app/hiddify.sh
@@ -8,8 +8,10 @@ mv hiddify-config /opt/
 
 sed -i "s/_DOMAIN_/${DOMAIN}/g" /opt/hiddify-config/config.env
 
-sed -i "/certbot/c\certbot --nginx -d ${DOMAIN} --non-interactive --agree-tos --dns-cloudflare --dns-cloudflare-credentials /etc/cf.ini  --https-port 444 --no-redirect"
+sed -i "/certbot/c\certbot --nginx -d ${DOMAIN} --non-interactive --agree-tos --dns-cloudflare --dns-cloudflare-credentials /etc/cf.ini  --https-port 444 --no-redirect" /opt/hiddify-config/nginx/run.sh
 
 sed -i "s/1004/1400/g" /opt/hiddify-config/nginx/common.conf.template
+
+cd /opt/hiddify-config || exit 1
 
 sudo bash install.sh
