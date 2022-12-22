8a62946bfd70f33dba004c0a6754bdb4db9ab7ad
Mohammad Fatemi <mr.smf8@gmail.com>
2022-12-23 01:44:42 +0330

    feat: Comment certbot cf plugin command change

--- app/hiddify.sh
+++ app/hiddify.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# commented because it's handled by calling CF API directly in setup.sh
+#sudo snap install certbot-dns-cloudflare
+
 git clone https://github.com/hiddify/hiddify-config
 
 cp app/hiddify-config.env hiddify-config/config.env
@@ -8,7 +11,7 @@ mv hiddify-config /opt/
 
 sed -i "s/_DOMAIN_/${DOMAIN}/g" /opt/hiddify-config/config.env
 
-sed -i "/certbot/c\certbot --nginx -d ${DOMAIN} --non-interactive --agree-tos --dns-cloudflare --dns-cloudflare-credentials /etc/cf.ini  --https-port 444 --no-redirect" /opt/hiddify-config/nginx/run.sh
+#sed -i "/certbot/c\certbot --nginx -d ${DOMAIN} --non-interactive --agree-tos --dns-cloudflare --dns-cloudflare-credentials /etc/cf.ini  --https-port 444 --no-redirect" /opt/hiddify-config/nginx/run.sh
 
 sed -i "s/1004/1400/g" /opt/hiddify-config/nginx/common.conf.template
 
